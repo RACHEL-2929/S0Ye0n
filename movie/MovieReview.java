0bb2d416e7f92ebf0050577744913680c5ecaff0
RACHEL-2929 <83777032+RACHEL-2929@users.noreply.github.com>
2024-03-10 20:54:12 +0900

    Delete movie directory

--- movie/MovieReview.java
+++
@@ -1,6 +0,0 @@
-package movie;
-//영화 리뷰정보 담기
-public class MovieReview {
-    String title;//영화제목
-    String review;//리뷰
-}
--- movie/PrintReviews.java
+++
@@ -1,9 +0,0 @@
-package movie;
-
-public class PrintReviews {
-    PrintReviews(MovieReview[] reviews){
-        for (MovieReview mR: reviews) {
-            System.out.println("영화 제목: " + mR.title + ", 리뷰:" + mR.review);
-        }
-    }
-}
